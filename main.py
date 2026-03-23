e40a8da10dfd00a4024b12ff69cffd8c9ae67314
Jacob Hart <hartj29@mcmsnj.net>
2026-03-23 14:57:06 -0400

    Added The Card Config

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+# CONFIG
+
+suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+
+values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": 11}
+
